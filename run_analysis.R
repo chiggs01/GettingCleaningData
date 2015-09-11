8c0ad4d90bcf8bf0f6ab3a0c8217d783c1603a07
chiggs01 <chiggs@no_reply.edu.au>
2015-09-11 12:55:26 +1000

    Aggregated data

--- run_analysis.R
+++ run_analysis.R
@@ -3,33 +3,39 @@ run_analysis <- function() {
       ##
       ##creates an independent tidy data set with the average of each variable for each activity and each subject.
       
-      ## Merge the training and test sets to create one data set
-      myData<-rbind(read.table("train/X_train.txt", colClasses="numeric", nrows = 5),
-                    read.table("test/X_test.txt", colClasses="numeric", nrows = 3))
-      myActivities<-rbind(read.table("train/y_train.txt", colClasses="numeric", nrows = 5),
-                          read.table("test/y_test.txt", colClasses="numeric", nrows = 3))
-      mySubjects<-rbind(read.table("train/subject_train.txt", colClasses="numeric", nrows = 5),
-                          read.table("test/subject_test.txt", colClasses="numeric", nrows = 3))
-
-      ## Extract measurements of mean and standard deviation for 
-      ## each measurement
+      # Merge the training and test sets to create one data set
+      myData<-rbind(read.table("train/X_train.txt", colClasses="numeric"),
+                    read.table("test/X_test.txt", colClasses="numeric"))
+      
+      # Extract measurements relating to the mean and standard deviation 
+      # from each measurement
       myColumns<-as.numeric(read.table("featureColumns.csv",sep=","))
       myData<-myData[,myColumns]
       
-      ## Label the data set with descriptive variable names 
+      # Label the data set with descriptive variable names 
       colTitles<-read.table("features.txt")
       names(myData)<-colTitles[myColumns,2]
-
-      ## Use descriptive activity names for activities in the data set 
-      activityLabels<-read.table("activity_labels.txt")
-      names(myActivities)[1]<-"Activity"
-      myActivities$Activity<-activityLabels[myActivities$Activity,2]
-
-      ## create an independent tidy data set with the average of each variable
-      ## for each activity and each subject
-      names(mySubjects)[1]<-"Subject"
-      myData<-cbind(myData,mySubjects,myActivities)
       
-      myData
+      # Add Activities and Subjects to the data set using descriptive 
+      # variable names
+      myActivities<-rbind(read.table("train/y_train.txt", colClasses="numeric"),
+                          read.table("test/y_test.txt", colClasses="numeric"))
+      mySubjects<-rbind(read.table("train/subject_train.txt", colClasses="numeric"),
+                        read.table("test/subject_test.txt", colClasses="numeric"))
+      names(myActivities)[1]<-"Activity"
+      names(mySubjects)[1]<-"Subject"
+      myData<-cbind(mySubjects, myActivities,myData)
+      
+      
+      # determine the average of each variable grouped around activity
+      # and subject
+      myData<-aggregate(myData, list(myData$Subject,myData$Activity), mean)
+      myData<-myData[-c(1:2)]
+      
+      # Use descriptive activity names for activities in the data set 
+      activityLabels<-read.table("activity_labels.txt")
+      myData$Activity<-activityLabels[myData$Activity,2]
+      
+      # create an independent tidy data set 
 }
 
